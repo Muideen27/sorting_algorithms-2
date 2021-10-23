3f173a795c2c421156186eda877b0b57204ae818
ralexrivero <ralexrivero@gmail.com>
2021-10-22 22:01:38 -0300

    bubble sort, git ignore and compile script

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,5 +6,22 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	unsigned long int i = 0, j = 0;
+	int *swap = NULL;
 
+	for (i = size; i > 0; i--)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (*(array + j) > *(array + j + 1))
+			{
+				swap = malloc(sizeof(int) * 1);
+				*swap = *(array + j);
+				*(array + j) = *(array + j + 1);
+				*(array + j + 1) = *swap;
+				print_array(array, size);
+				free(swap);
+			}
+		}
+	}
 }
