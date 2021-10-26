c79d33241eab723c1725c1db051639a0742b18cf
ralexrivero <ralexrivero@gmail.com>
2021-10-25 23:03:11 -0300

    simplify bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,7 +7,7 @@
 void bubble_sort(int *array, size_t size)
 {
 	unsigned long int i = 0, j = 0;
-	int *swap = NULL;
+	long int swap = 0;
 
 	if (size < 2) /*if array is less than 2 not need to be sorted*/
 	return;
@@ -18,12 +18,10 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (*(array + j) > *(array + j + 1))
 			{
-				swap = malloc(sizeof(int) * 1);
-				*swap = *(array + j);
+				swap = *(array + j);
 				*(array + j) = *(array + j + 1);
-				*(array + j + 1) = *swap;
+				*(array + j + 1) = swap;
 				print_array(array, size);
-				free(swap);
 			}
 		}
 	}
--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,7 +5,7 @@
 * 88 88 Y88 o.`Y8b 88""   88"Yb    88   88 Yb   dP 88 Y88
 * 88 88  Y8 8bodP' 888888 88  Yb   88   88  YbodP  88  Y8
 */
-
+void swap_dll(listint_t *current, listint_t *insertion, listint_t **head);
 /**
  * insertion_sort_list - sort a doubly linked list of integer
  * in ascending order
@@ -13,15 +13,53 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	long int i = 0, j = 0;
-	listint_t *swap = NULL, *current = NULL;
+	listint_t *current = NULL, *insertion = NULL, *head = NULL;
 
-	current = malloc(sizeof(listint_t));
-	current = list;
+	current = (*list)->next;
+	head = *list;
+
+	if (list == NULL || *list == NULL)
+		return;
 
 	while (current != NULL)
-	{
-		printf("current: %d", current->n);
-		/* step to move forward */
-		current->next;
+	{	/* start in head every loop until find current */
+		insertion = head; /* insertion pointer */
+		/* compare current value with insertion point */
+		/* iterates trowgh find current o swap conditional */
+		while (insertion != current)
+		{
+			if (insertion->n > current->n)
+			{
+				/* swap links between nodes */
+				swap_dll(current, insertion, list);
+				/* move forward current */
+				current = current->next;
+			}
+			insertion = insertion->next;
+		}
+		current = current->next;
 	}
+}
+/**
+ * swap_dll - swap two values in a doubly linked list
+ * @current: current node been evaluated for insertion sort
+ * @insertion: insertion point where move `current` node
+ * @head: head pointer to the list
+ */
+void swap_dll(listint_t *current, listint_t *insertion, listint_t **head)
+{
+	listint_t *swap1 = current->next;
+	listint_t *swap2 = insertion->prev;
+
+	if (swap1 != NULL)
+		swap1->prev = insertion;
+	if (swap2 != NULL)
+		swap2->next = current;
+	current->prev = swap2;
+	insertion->next = swap1;
+	current->next = insertion;
+	insertion->prev = current;
+	if (*head == insertion)
+		*head = current;
+	print_list(*head);
+}
